3a5990da2e5dd329c827246f892690b67d50b116
Claire Nord <cnord@mit.edu>
2019-12-17 00:58:34 -0500

    setup: format with black

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup
 
 setup(
-    name='auto-selfcontrol',
-    version='1.0',
-    description='Small utility to schedule start and stop times of SelfControl',
-    url='github.com/andreasgrill/auto-selfcontrol',
-    long_description=open('README.md').read(),
-    install_requires=["pyobjc", "pyobjc-core"]
+    name="auto-selfcontrol",
+    version="1.0",
+    description="Small utility to schedule start and stop times of SelfControl",
+    url="github.com/andreasgrill/auto-selfcontrol",
+    long_description=open("README.md").read(),
+    install_requires=["pyobjc", "pyobjc-core"],
 )
